cec8bc62b65abe2bb5f3b4c21b6a1b6e1edcd18d
jeremyHarrell <hillvulf@gmail.com>
2022-12-26 09:54:53 -0600

    added python adventure and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css'
 import App from './App'
 
