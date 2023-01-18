18c7402d08a4c6816c480010a9656217b1271d69
jeremyHarrell <hillvulf@gmail.com>
2023-01-18 12:52:59 -0600

    attempting to push to railway

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,58 +1,19 @@
 import React from 'react'
-// import Nav from 'react-bootstrap/Nav';
 import { Navbar, Image } from 'react-bootstrap';
 import JeremyPhotoNoText from '../assets/JeremyPhotoNoText.png'
 
 const NavBar = () => {
 
-  // const handleClick = () => {
-  //   fetch('JeremyHarrellUpdatedResume.pdf').then(response => {
-  //     response.blob().then(blob => {
-  //       const fileURL = window.URL.createObjectURL(blob)
-  //       let alink = document.createElement('a');
-  //       alink.href = fileURL;
-  //       alink.download = 'JeremyHarrellUpdatedResume.pdf';
-  //       alink.click()
-  //     })
-  //   })
-  // }
-
 
   return (
     <header>
       <Navbar bg="dark" expand="lg" variant='dark'id='header' >
-        {/* <Container> */}
-          {/* <Navbar.Brand href="/" ><h1>Jeremy Harrell</h1> <h5 className='title'>Software Engineer</h5></Navbar.Brand> */}
-          {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
           <Navbar id="basic-navbar-nav"className="justify-content-end">
-            {/* <Nav >
-              <Nav.Link href="/projects">Projects</Nav.Link> */}
-              {/* <Nav.Link href="/projects-copy">ProjectsCopy</Nav.Link> */}
-              {/* <Nav.Link onClick={handleClick}>Resume</Nav.Link>
-            </Nav> */}
             <Image className="navImage" id='navImage' src={JeremyPhotoNoText} alt="man staring at camera" roundedCircle></Image>
           </Navbar>
-        {/* </Container> */}
       </Navbar>
     </header>
   );
 }
 
 export default NavBar
-
-// <div>
-// <div className='navMain'>
-//   <Link to='/' className='text-3xl font-bold underline'>
-//     Home
-//   </Link>
-//   <Link to='/projects' className='text-3xl font-bold underline'>
-//     Projects
-//   </Link>
-//   <Link to='/about' className='text-3xl font-bold underline'>
-//     About
-//   </Link>
-//   <Link to='/resume' className='text-3xl font-bold underline'>
-//     Resume
-//   </Link>
-// </div>
-// </div>
--- src/components/PortfolioHome.jsx
+++ src/components/PortfolioHome.jsx
@@ -19,7 +19,6 @@ const PortfolioHome = () => {
     <Container fluid="md">
       <Row>
         <Stack direction="horizontal" gap={2}>
-          {/* <Image className="float-left mt-5 mb-5"src={JeremyPhotoNoText} alt="man staring at camera" rounded style={{ display: "flex", width:500, padding: 30,}}></Image> */}
             <Col><h1 className='myPersonalInfo text-5xl font-light text-center mt-5' > Hi, I'm Jeremy Harrell.</h1> 
             <h4>I am an Austin, Texas based junior software developer, musician and artist. I approach all aspects of my life with a passion for creativity and functionality.</h4>
               <h5>My professional experience spans the hospitality, start-up, and e-commerce industries. As an operations and people manager of a team of 25, I have a proven track record in solving problems and trouble shooting on the fly, and understand the importance of planning ahead and effective communication. I would like to use my skills to be a part of a creative, solutions-oriented team that works hard and has fun doing it.</h5>
@@ -27,13 +26,6 @@ const PortfolioHome = () => {
               </Col>
         </Stack>
       </Row>
-      {/* <Row>
-        <Stack direction="horizontal" gap={3}>
-            <Link to='/about' className='text-2xl font-bold underline mb-5'>
-            More About Me
-          </Link>
-        </Stack>
-      </Row> */}
     </Container>
   )
 }
